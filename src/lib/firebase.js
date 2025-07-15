1781a966bdb8a518261b3c914e0e910f3401b0c1
hafriaya <ms.hafriaya@gmail.com>
2025-07-15 19:59:14 +0100

    init_auth_config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +18,14 @@ const firebaseConfig = {
   measurementId: "G-6XFG8R8RJ7"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+// Only initialize analytics in the browser
+let analytics = null;
+if (typeof window !== "undefined") {
+  analytics = getAnalytics(app);
+}
+
+const auth = getAuth(app);
+
+export { app, analytics, auth };
